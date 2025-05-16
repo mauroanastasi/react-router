82645cc5d717370f0f0e4b4c0cf9f6b73b8c0760
mauroanastasi <mauroanastasi1@gmail.com>
2025-05-16 19:22:53 +0200

    [UPD] creo la card per la visualizzazione prodotti, renderizzata col map sulla variabile di stato

--- pages/Products.jsx
+++ pages/Products.jsx
@@ -14,6 +14,42 @@ const Products = () => {
 
     return (
         <>
+            <MainNavbar />
+            <div>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-12">
+                            <h1>Lista prodotti</h1>
+                        </div>
+                    </div>
+
+                    {products === null ? (
+                        <div>Loading..</div>
+                    ) : (
+                        <div className="row g-3">
+                            {products.map((product) => {
+                                return (
+                                    <div className="col-12 col-md-6 col-lg-4" key={product.id} >
+                                        <div className="card">
+                                            <div className="card-image-top">
+                                                <img src={product.image} className="img-fluid" alt="" />
+                                            </div>
+                                            <div className="card-body">
+                                                <h3>{product.title}</h3>
+
+                                                <p>{product.price}</p>
+                                                <p>{product.description}</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                );
+                            })}
+                        </div>
+                    )}
+
+                </div>
+
+            </div>
 
         </>
     )
